9ad268221be4d6fb328a47b1ad0e3a0d4d0d84cd
Shailza Dubey <shailza13@gmail.com>
2020-09-19 15:51:55 +0530

    Update BookStoreApp.java

--- lab2_shailza/src/q2/BookStoreApp.java
+++ lab2_shailza/src/q2/BookStoreApp.java
@@ -6,57 +6,52 @@ public class BookStoreApp {
 
 	public static void main(String[] args) {
 		BookStore bs = new BookStore();
+		Scanner sc = new Scanner(System.in);
 		
 		
-		int choice=0;
 		do {
 			
 			System.out.println(" Main Menu ");
 			System.out.println(" 1 - Display all ");
 			System.out.println(" 2 - Order books ");
 			System.out.println(" 3 - Sell books ");
-			System.out.println(" 4 - Exit ");
-			int noOfCopies=0;
-			String isbn,bookTitle,c;
-			
+			System.out.println(" 0 - Exit ");
 			System.out.println("Enter your choice!");
-			Scanner sc = new Scanner(System.in);
-			choice=sc.nextInt();
+			
+			int choice=sc.nextInt();
+			
+			
 			switch(choice) {
 			case 1 : bs.display();
 			         break;
-			case 2 : a:{System.out.println("Enter the book title and no of copies you want to order");
-			         isbn=sc.next();
-			         noOfCopies=sc.nextInt();
-			         try {
-						bs.order(isbn,noOfCopies);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			         System.out.println("Do you want to order again? Y/N");
-			         c=sc.next();
-			         if(c=="y"||c=="Y")
-			        	 break a;
-			         }
 			         
+			case 2 : System.out.print("Enter the book isbn: ");
+			         sc.nextLine();
+			         String isbn=sc.nextLine();
+			         System.out.print("Enter the no of copies you want to order: ");
+			         int noOfCopiesOrder=sc.nextInt();
+			         bs.order(isbn,noOfCopiesOrder);
 			         break;
-			case 3 : b:{System.out.println("Enter the book title and no of copies you want to order");
-	                     bookTitle=sc.next();
-	                     noOfCopies=sc.nextInt();
-	                     bs.sell(bookTitle,noOfCopies);
-	                     System.out.println("Do you want to sell again? Y/N");
-	                     c=sc.next();
-	                     if(c=="y"||c=="Y")
-	                    	 break b;}
-	                     
-	                     break;
-			 case 4 :  break;
+
+			         
+			  
+			case 3 : System.out.print("Enter the book title: ");
+			         sc.nextLine();
+				 String bookTitle=sc.nextLine();
+				 System.out.print("Enter the no of copies you want to sell: ");
+				 int noOfCopiesSell=sc.nextInt();
+				 bs.sell(bookTitle,noOfCopiesSell);
+				 break;
+
+	                 
+	                    	 
+	               case 0 : break;
 			
 		}
 		
 
 	}while(true);
+    
+   }
+}
 
-}
-}
